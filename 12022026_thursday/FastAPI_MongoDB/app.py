b5e39f6bef43f57fbe27c8129c1d5ca1b7258434
om151204 <ommishra97277@gmail.com>
2026-02-12 12:18:35 +0530

    Add:Created a FastAPI post endpoint using MongoDB

---
+++ 12022026_thursday/FastAPI_MongoDB/app.py
@@ -0,0 +1,49 @@
+from pymongo import MongoClient
+from fastapi import FastAPI,status
+from pydantic import BaseModel, StrictStr,StrictInt, StrictFloat,Field
+from typing import List
+
+app = FastAPI()
+
+def connect_mongodb():
+    """
+    Connect to MongoDB
+    :return: Collections
+    """
+    try:
+        client = MongoClient("mongodb://localhost:27017/")
+        print("Connected to MongoDB")
+
+        database = client.get_database("Om")
+        collection = database["fastAPI_mongoDB_tutorial"]
+        return collection
+
+    except ConnectionError as e:
+        print("Connection Error: ",e)
+
+class Student(BaseModel):
+    name: StrictStr
+    age: StrictInt = Field(gt = 0,lt = 30)
+    gender: StrictStr
+    marks:List[StrictFloat] = List[Field(gt = 0,lt = 100)]
+
+@app.post("/student",status_code=status.HTTP_201_CREATED)
+def add_student(req_data: Student):
+    collection = connect_mongodb()
+    total_students = [i for i in collection.find()]
+    _id = len(total_students) + 1
+    collection.insert_one({
+        "_id": _id,
+        "name":req_data.name,
+        "age":req_data.age,
+        "gender":req_data.gender,
+        "marks":req_data.marks,
+    })
+    return f"Student {req_data.name} added successfully with id {_id}"
+
+
+
+
+
+
+
