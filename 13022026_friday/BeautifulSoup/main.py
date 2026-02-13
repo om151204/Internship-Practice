10b12dc000b5c1b5d0bb4d260b62bbcd92c8e3e1
om151204 <ommishra97277@gmail.com>
2026-02-13 14:15:33 +0530

    Update: Implemented different extraction methods

--- 13022026_friday/BeautifulSoup/main.py
+++ 13022026_friday/BeautifulSoup/main.py
@@ -11,10 +11,47 @@ def basic_fetching():
 def parsing_using_beautifulsoup():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup.title)
-    print(soup.title.text)
+    # print(soup.title)
+    # print(soup.title.text,"\n\n")
+    return soup
+
+def finding_elements(soup):
+    single_h3 = soup.find("h3")
+    print(single_h3)
+    all_h3 = soup.find_all("h3")
+    print(all_h3)
+    section_id = soup.find_all("section" ,id="quick-start")
+    for texts in section_id:
+        print(texts.text)
+
+def extracting_attributes(soup):
+    links = soup.find_all("a")
+    for link in links:
+        print(link.get("href"))
+
+
+def store_data_in_list(soup):
+    page_headings = []
+    main_headings = soup.find_all("h1")
+    sub_headings = soup.find_all("h2")
+    for heading,subheading in zip(main_headings,sub_headings):
+        page_headings.append({
+            "main_heading": heading.text,
+            "sub_heading": subheading.text
+        })
+    print(page_headings)
 
 
 
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    data = parsing_using_beautifulsoup()
+    store_data_in_list(data)
 
 
