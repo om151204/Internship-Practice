f68f9262e522e5fe631e99060c63df9e3fb71ed8
om151204 <ommishra97277@gmail.com>
2026-02-13 17:23:07 +0530

    Update: Practiced different method

--- 13022026_friday/BeautifulSoup/main.py
+++ 13022026_friday/BeautifulSoup/main.py
@@ -4,7 +4,9 @@ from bs4 import BeautifulSoup
 import requests
 from sqlalchemy.orm.base import CALLABLES_OK
 
-url = "https://www.crummy.com/software/BeautifulSoup/bs4/doc/"
+# url = "https://www.crummy.com/software/BeautifulSoup/bs4/doc/"
+url = "https://beautiful-soup-4.readthedocs.io/en/latest/"
+second_url = "template.html"
 
 def basic_fetching():
     response = requests.get(url)
@@ -12,6 +14,13 @@ def basic_fetching():
     print(response.text[:500])   # The datatype of response is string
 
 def parsing_using_beautifulsoup():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # print(soup.title)
+    # print(soup.title.text,"\n\n")
+    return soup
+
+def parsing_using_beautifulsoup_second():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     # print(soup.title)
@@ -67,21 +76,38 @@ def link_extractor(soup):
         full_href = urljoin(url, href)
         print(full_href)
 
+def image_extractor(soup):
+    images = soup.find_all("img")
+    for image in images:
+        print(image.get("src"))
+        print(image.get("alt"))
 
+def get_table(soup):
+    for table in soup.find_all("table"):
+        rows = table.find_all("tr")
+        for row in rows:
+            cols = row.find_all(["td","th"])
+            data = [ele.text.strip() for ele in cols]
+            print(f"Table Row {data}")
 
-
-
-
-
-
-
-
-
+def add_new_element(soup):
+    ul_tag = soup.new_tag("ul")
+    li_tag = soup.new_tag("li")
+    li_tag.string = "Home"
+    ul_tag.append(li_tag)
+    li_tag = soup.new_tag("li")
+    li_tag.string = "Work"
+    ul_tag.append(li_tag)
+    soup.html.body.insert(0,ul_tag)
+    with open("template.html","w",encoding="utf-8") as f:
+        f.write(str(soup))
 
 
 
 if __name__ == "__main__":
     data = parsing_using_beautifulsoup()
-    link_extractor(data)
+    data2 = parsing_using_beautifulsoup_second()
+    add_new_element(data2)
+
 
 
