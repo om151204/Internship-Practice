0d7a90092508e96a15a46b327d13eb8ebdee288c
om151204 <ommishra97277@gmail.com>
2026-03-25 17:02:55 +0530

    Add: Main file to create and manage agent workflow

---
+++ 25032026_wednesday/BA_automation_system/main.py
@@ -0,0 +1,30 @@
+from pdf_loader import load_pdf
+from agent import create_agent
+
+
+def run(pdf_path):
+    text = load_pdf(pdf_path)
+    agent = create_agent()
+
+    query = f"""
+    Follow this sequence strictly:
+    1. requirements_tool
+    2. user_story_tool
+    3. task_tool
+
+    input:
+    {text}
+
+    Final Output must include:
+    - Requirements
+    - User Stories
+    - Tasks 
+
+    """
+    result = agent.run(query)
+    return result
+
+if __name__ == "__main__":
+    output = run("SRS.pdf")
+    print("\nFINAL OUTPUT:- ")
+    print(output)
