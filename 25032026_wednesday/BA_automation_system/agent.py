1881398f9e84f83109b7920216789c5892bb4a2f
om151204 <ommishra97277@gmail.com>
2026-03-27 14:42:43 +0530

    Update: Changed the agent to create_agent

--- 25032026_wednesday/BA_automation_system/agent.py
+++ 25032026_wednesday/BA_automation_system/agent.py
@@ -1,8 +1,8 @@
-from langchain_classic.agents import initialize_agent, AgentType
+from langchain.agents import create_agent
 from config import llm
 from tools import requirements_tool,user_story_tool,task_tool
 
-def create_agent():
+def agent_creation():
     """
     Create an agent on three tools and return it
     :return: None
@@ -10,14 +10,25 @@ def create_agent():
     try:
         tools = [requirements_tool,user_story_tool,task_tool]
 
-        agent = initialize_agent(
+        agent = create_agent(
+            model = llm,
             tools = tools,
-            llm = llm,
-            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-            verbose = True
+            system_prompt = """
+            You are a Business Analyst Agent.
+        Workflow:
+        1. Generate functional and non-functional requirements from the provided document.
+        2. Generate detailed user stories based on the requirements.
+        3. Generate implementation tasks from the user stories.
+        
+        Rules:
+        - Use only the provided document content.
+        - Do not invent missing business logic.
+        - Always call tools in order: generate_requirements -> generate_user_stories -> generate_tasks
+    """
         )
-
         return agent
     except Exception as e:
         print("Error in creating agent",e)
+
+
 
